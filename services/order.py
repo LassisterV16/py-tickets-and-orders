592e3170eecc1cf18fb0db47b2e54e7749d3f92d
lassisterv16 <lassisterv16@gmail.com>
2026-03-19 22:11:58 +0100

    fixed date

--- services/order.py
+++ services/order.py
@@ -11,13 +11,12 @@ def create_order(
     username: str,
     date: str = None,
 ) -> None:
-    order = Order.objects.create(
-        user=get_user_model().objects.get(username=username)
-    )
+    user = get_user_model().objects.get(username=username)
+    order = Order.objects.create(user=user)
 
     if date:
-        order.created_at = date
-        order.save()
+        Order.objects.filter(pk=order.pk).update(created_at=date)
+        order.refresh_from_db()
 
     for ticket in tickets:
         Ticket.objects.create(
