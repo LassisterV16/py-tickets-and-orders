fa95f574d9e7607b3be0ae8d248873c11c09826a
lassisterv16 <lassisterv16@gmail.com>
2026-03-19 22:39:27 +0100

    fixed date v2

--- services/order.py
+++ services/order.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db import transaction
 from django.db.models import QuerySet
+from datetime import datetime
 
 from db.models import Order, Ticket
 
@@ -10,13 +11,22 @@ def create_order(
     tickets: list[dict],
     username: str,
     date: str = None,
-) -> None:
+) -> Order:
     user = get_user_model().objects.get(username=username)
-    order = Order.objects.create(user=user)
+    order_data = {"user": user}
 
     if date:
-        Order.objects.filter(pk=order.pk).update(created_at=date)
-        order.refresh_from_db()
+        parsed_date = datetime.strptime(date, "%Y-%m-%d %H:%M")
+        field = Order._meta.get_field("created_at")
+        field.auto_now_add = False
+
+        try:
+            order_data["created_at"] = parsed_date
+            order = Order.objects.create(**order_data)
+        finally:
+            field.auto_now_add = True
+    else:
+        order = Order.objects.create(**order_data)
 
     for ticket in tickets:
         Ticket.objects.create(
@@ -25,6 +35,8 @@ def create_order(
             row=ticket["row"],
             seat=ticket["seat"]
         )
+
+    return order
 
 
 def get_orders(username: str = None) -> QuerySet[Order]:
